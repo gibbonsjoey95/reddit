028a19bac929e549664bf3b4f7493729df7076bc
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-08-08 14:06:36 -0500

    finished reddit clone

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+
+  validates :name, :username, :email, presence: true
+  validates :username, :email, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_08_031535) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_185340) do
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.text "username"
     t.string "email"
     t.datetime "created_at", null: false
